5b1a7cd147175c0c8b1ca8c33da050aa462d2995
anandp926 <sanand926@gmail.com>
2019-03-01 00:53:12 +0530

    added build for testing

---
+++ docs/precache-manifest.1d2a1416317774c1537788af7815b175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ea249ce110213237cda9",
+    "url": "/static/js/main.ea249ce1.chunk.js"
+  },
+  {
+    "revision": "b2a18b31e389d313809d",
+    "url": "/static/js/2.b2a18b31.chunk.js"
+  },
+  {
+    "revision": "ea249ce110213237cda9",
+    "url": "/static/css/main.306cb9a7.chunk.css"
+  },
+  {
+    "revision": "347acf635c0bdb7622585363853b0a62",
+    "url": "/index.html"
+  }
+];
